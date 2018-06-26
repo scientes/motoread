0e9e2e9ba74e84d846ffb653a5929ec95680b4b6
scientes <scientes12@gmail.com>
2018-06-26 21:44:43 +0200

    added pycharm project added slim venv added imagehashing to reduce cpu load

--- test.py
+++ test.py
@@ -4,7 +4,10 @@ from PIL import Image
 import PIL.ImageOps
 import pytesseract
 import PIL
-from picamera import PiCamera
+import os
+import imagehash
+import time
+#from picamera import PiCamera
 import pyttsx3 as tts
 def trim(im):
     from PIL import Image, ImageChops
@@ -16,34 +19,65 @@ def trim(im):
         return im.crop(bbox)
 
 
-
+"""
 my_stream = BytesIO()
 camera = PiCamera()
 camera.start_preview()
 sleep(15)
 camera.capture(my_stream, 'png')
 print("hi")
+"""
+def computehash(image_stream):
+    dimage=imagehash.dhash(image_stream)
+def image_opt(my_stream):
+    image=Image.open(my_stream)
+    #image.save("test2.png")
 
-image=Image.open(my_stream)
-image.save("test2.png")
+    im = image.convert("L")
+    #im.show()
+    im = PIL.ImageOps.autocontrast(im, 0.8)
+    th = 120  # the value has to be adjusted for an image of interest
 
-im = image.convert("L")
-im.show()
-im = PIL.ImageOps.autocontrast(im, 0.8)
-th = 150  # the value has to be adjusted for an image of interest
+    im = im.point(lambda i: i < th and 255)
 
-im = im.point(lambda i: i < th and 255)
+    im= trim(im)
 
-im = trim(im)
-width = im.size[0]
-height = im.size[1]
-im = im.crop((0, int((height * 0.5) / 2), width, height - int(height * 0.5 / 2)))
-im.show()
-text = pytesseract.image_to_string(im)
-print(text)
-engine=tts.init()
-#voices = engine.getProperty('voices')
-#engine.setProperty('voice', "german")
-engine.say(text)
-engine.runAndWait()
-#im.save("test.png")
+    width = im.size[0]
+    height = im.size[1]
+    a=(int((width * 0.2) / 2), int((height * 0.5) / 2), width-int((height * 0.3) / 2), height - int((height * 0.5) / 2))
+    return im.crop(a)
+    #im.show()
+    """text = pytesseract.image_to_string(im)
+    print(text)
+    engine=tts.init()
+    #voices = engine.getProperty('voices')
+    #engine.setProperty('voice', "german")
+    engine.say(text)
+    engine.runAndWait()
+    #im.save("test.png")
+    """
+if __name__=="__main__":
+    lis=os.listdir("motomed_live")
+    for i in range(len(lis)):
+        lis[i]="motomed_live/"+lis[i]
+    hashlist=[]
+    star1=time.clock()
+    for i in range(len(lis)):
+        start=time.clock()
+        image=image_opt(lis[i])
+        ihash=imagehash.dhash(image,hash_size=32)
+        hashlist.append(ihash)
+        end=time.clock()
+        if i>0:
+            distance=ihash-oldhash
+            print(i, " time taken: ", end - start, "s fps:", 25 / (end - start), " distance:", distance)
+            if distance>30:
+                #image.show()
+                #sleep(0.5)
+                oldhash = ihash
+
+        else:
+            oldhash=ihash
+
+    end1=time.clock()
+    print(len(lis),end1-star1,len(lis)/(end1-star1))
